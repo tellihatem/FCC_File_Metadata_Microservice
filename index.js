3b611bfb7973e8d3e859f340a88ef7e0f036dc2c
Hatem Telli <thoverdz@gmail.com>
2024-07-09 12:21:04 +0000

    ALL

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
@@ -11,7 +12,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Set up multer for file storage
+const storage = multer.memoryStorage(); // You can configure storage options here
+const upload = multer({ storage: storage });
 
+app.post('/api/fileanalyse' , upload.single('upfile'), (req , res) =>{
+  res.json({
+    name : req.file.originalname,
+    type : req.file.mimetype,
+    size : req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
